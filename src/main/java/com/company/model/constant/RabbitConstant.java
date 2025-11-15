4f233ab5700d6381c23342364d044596bdf1363b
nijat <nicatxanaliev@gmail.com>
2025-11-15 20:53:04 +0400

    RabbitConstants created

---
+++ src/main/java/com/company/model/constant/RabbitConstant.java
@@ -0,0 +1,14 @@
+package com.company.model.constant;
+
+public final class RabbitConstant {
+
+    // QUEUE
+    public static final String STOCK_QUEUE = "stock-queue";
+
+    // EXCHANGE
+    public static final String STOCK_EXCHANGE = "stock-exchange";
+
+    // ROUTING KEY
+    public static final String STOCK_ROUTING_KEY = "stock.order.created";
+
+}
